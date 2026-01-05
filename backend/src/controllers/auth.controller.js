5a4dd3b875fed37661567a2d5c9d9dff22c6fb08
CrazyDevJohn <crazydevjohn@gmail.com>
2026-01-06 03:14:10 +0530

    implimenting deployment complete

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -180,7 +180,7 @@ export const getUserProfile = async (req, res) => {
 
 export const logout = async (req, res) => {
   try {
-    await res.cookies('AKURATAMA_ICT_REGISTER_TOKEN', '');
+    await res.cookie('AKURATAMA_ICT_REGISTER_TOKEN', '');
     return res.status(200).json({
       status: true,
       message: 'logout success!',
