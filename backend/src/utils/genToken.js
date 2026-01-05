9c92ea9d78bf55f1fe2666d3a9d8c6a4b6714da3
CrazyDevJohn <crazydevjohn@gmail.com>
2026-01-06 01:43:28 +0530

    implimenting deployment complete

--- backend/src/utils/genToken.js
+++ backend/src/utils/genToken.js
@@ -6,8 +6,8 @@ export const genToken = async (res, payload, secret, exp) => {
   });
 
   return res.cookie('AKURATAMA_ICT_REGISTER_TOKEN', token, {
-    maxAge: exp === '15d' ? 15 * 24 * 60 * 60 * 1000 : 15 * 60 * 1000, // 7D MS
-    httpOnly: false,
+    maxAge: 7 * 24 * 60 * 60 * 1000, // 7D MS
+    httpOnly: true,
     sameSite: 'strict',
     secure: true,
   });
