e087a5e6035d8d384c9b8691c173d7b1a6fa1676
CrazyDevJohn <crazydevjohn@gmail.com>
2026-02-08 22:51:00 +0530

    implimenting deployment complete

--- client/database/api-calls.js
+++ client/database/api-calls.js
@@ -1,6 +1,6 @@
 const createUserWithEmailAndPassword = async (name, email, password) => {
   const res = await axios.post(
-    `http://localhost:5000/api/v2/auth/register`,
+    `https://akuratama-ict-frelancer-projact.vercel.app/api/v2/auth/register`,
     {
       name,
       email,
@@ -8,7 +8,7 @@ const createUserWithEmailAndPassword = async (name, email, password) => {
     },
     {
       withCredentials: true,
-    }
+    },
   );
 
   return await res.data;
@@ -16,53 +16,50 @@ const createUserWithEmailAndPassword = async (name, email, password) => {
 
 const logiWithEmailAndPasword = async (email, password) => {
   const res = await axios.post(
-    `http://localhost:5000/api/v2/auth/login`,
+    `https://akuratama-ict-frelancer-projact.vercel.app/api/v2/auth/login`,
     {
       email,
       password,
     },
     {
       withCredentials: true,
-    }
+    },
   );
 
-  
-  console.log(res.data.user)
+  console.log(res.data.user);
   return await res.data;
 };
 
 const sendOtp = async (otp) => {
   const res = await axios.post(
-    `http://localhost:5000/api/v2/auth/verify`,
+    `https://akuratama-ict-frelancer-projact.vercel.app/api/v2/auth/verify`,
     {
       otp: otp,
     },
     {
       withCredentials: true,
-    }
+    },
   );
-
 
   return await res.data;
 };
 
 const checkIsAuthonticated = async () => {
   const res = await axios.get(
-    `http://localhost:5000/api/v2/auth/profile`, 
+    `https://akuratama-ict-frelancer-projact.vercel.app/api/v2/auth/profile`,
     {
       withCredentials: true,
-     
-    }
+    },
   );
-
 
   return await res.data;
 };
 
 const logOut = async () => {
   const res = await axios.post(
-    `http://localhost:5000/api/v2/auth/logout`,
-    {}, {withCredentials:true}
+    `https://akuratama-ict-frelancer-projact.vercel.app/api/v2/auth/logout`,
+    {},
+    { withCredentials: true },
   );
 
   console.log(res.data);
@@ -70,54 +67,55 @@ const logOut = async () => {
   return await res.data;
 };
 
-
-const getCourseById= async (id) => {
+const getCourseById = async (id) => {
   const res = await axios.get(
-    `http://localhost:5000/api/v2/course/id/${id}`,
-     {withCredentials:true}
+    `https://akuratama-ict-frelancer-projact.vercel.app/api/v2/course/id/${id}`,
+    { withCredentials: true },
   );
 
   console.log(res.data);
 
   return await res.data;
-}; 
+};
 
 const fetchAllCourses = async () => {
-  const res = await axios.get("http://localhost:5000/api/v2/course", {withCredentials:true});
+  const res = await axios.get(
+    "https://akuratama-ict-frelancer-projact.vercel.app/api/v2/course",
+    { withCredentials: true },
+  );
   console.log("All courses ", await res.data.courses);
-  return await res.data.courses
-}
+  return await res.data.courses;
+};
 
 const fetchAllCoursesForStudent = async () => {
-  const res = await axios.get("http://localhost:5000/api/v2/course/all-enrolled", {withCredentials:true});
+  const res = await axios.get(
+    "https://akuratama-ict-frelancer-projact.vercel.app/api/v2/course/all-enrolled",
+    { withCredentials: true },
+  );
   console.log("All courses ", await res.data.course);
-  return await res.data.course
-}
+  return await res.data.course;
+};
 
 const uploadBill = async (file, data) => {
-        try {
-          const formData = new FormData();
-          formData.append("file", file);
-          formData.append("bill", JSON.stringify(data));
+  try {
+    const formData = new FormData();
+    formData.append("file", file);
+    formData.append("bill", JSON.stringify(data));
 
-          const res = await axios.post(
-            "http://localhost:5000/api/v2/billing/billing",
-            formData,
-            {
-              withCredentials: true,
-              
-            }
-          );
+    const res = await axios.post(
+      "https://akuratama-ict-frelancer-projact.vercel.app/api/v2/billing/billing",
+      formData,
+      {
+        withCredentials: true,
+      },
+    );
 
-          console.log(res);
-
-
-        } catch (error) {
-          console.log("error in uploading billl");
-          console.log(error);
-        }
+    console.log(res);
+    return await res.data;
+  } catch (error) {
+    console.log("error in uploading billl");
+    console.log(error);
+  }
 };
-      
-
 
 // https://akuratama-ict-frelancer-projact.vercel.app/api/v2
